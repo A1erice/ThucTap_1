cd199588d35a725d17124ab3d34060333923efaa
A1erice <128226905+A1erice@users.noreply.github.com>
2024-10-02 13:54:18 +0700

    test

---
+++ ThucTap_1/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ThucTap_1
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+    }
+}
